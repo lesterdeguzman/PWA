dd90c7f4589be80c2826a09eed56aa44d49c8c46
John Michael <mja_095@hotmail.com>
2019-10-14 13:56:34 +0800

    Updated js script
    still needs improvement

--- e-Task/script/script.js
+++ e-Task/script/script.js
@@ -11,7 +11,7 @@ function addCard() {
 }
 
 function addList() {
-  var div = document.getElementById("div-list");
+  var div = document.getElementById("lists-section");
   var inDiv = document.createElement("div");
   var inner = '<h3 class="list-title">Tasks to Do</h3><ul class="list-items" id="tasks"><li>1.1 Create new card </li><li>1.2 Create new list</li><li>1.3 Create new board</li><li>2. Upload files</li><li>3. Create new board</li></ul><button class="add-card-btn btn" onClick="addCard()">Add a card</button>';
   inDiv.setAttribute("class", "list");
